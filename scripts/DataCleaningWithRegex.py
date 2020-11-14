b3fc32815e0d9f282d30240be5d56b6af8c2456e
Alex Ginglen <Alex@Alexs-MBP.zyxel.com>
2020-11-14 15:57:43 -0600

    updated cleaning script

--- scripts/DataCleaningWithRegex.py
+++ scripts/DataCleaningWithRegex.py
@@ -2,40 +2,24 @@ import csv
 import re
 from joblib import Parallel, delayed
 import multiprocessing
+import pandas as pd
 
-regexes = ['@\d{3,9}','^(https?|ftp)://[^\s/$.?#].[^\s]*$','&amp;','\/[A-Z]{2}','\^[A-Z]{2}','https://t\..{2,100}\s','https://t\..{1,3}\/[1-9a-zA-Z]{1,100}\s']
-regexReplaceWithQuote = ['https://t\..{2,100}\"','https://t\..{1,3}\/[1-9a-zA-Z]{1,100}\"']
-regexReplaceWithComma = ['https://t\..{2,100}\,','https://t\..{1,3}\/[1-9a-zA-Z]{1,100}\,']
-
-def removeEmoji(line):
-    item = line
-    for regex in regexes:
-        item = re.sub(regex, '', item.rstrip())
-    for regex in regexReplaceWithQuote:
-        item = re.sub(regex, '"', item.rstrip())
-    for regex in regexReplaceWithComma:
-        item = re.sub(regex, ',', item.rstrip())
-    item = item + '\n'
-    return item
-
-text = open("twcs.csv", "r")
-
-numberOfLines = sum(1 for line in open("twcs.csv", "r"))
-print(numberOfLines)
+regexes = ['@\d{3,9}','^(https?|ftp)://[^\s/$.?#].[^\s]*$','&amp;','^(https?|ftp)://[^\s/$.?#].[^\s]*$','\/[A-Z]{2}','\^[A-Z]{2}','https://t\..{2,100}','https://t\..{1,3}\/[1-9a-zA-Z]']
 
 
+translatedEmoji = pd.read_csv('twcs-translatedEmoji.csv')
 
-x = open("output-RemoveRegex.csv","w")
+
+print(translatedEmoji.head())
 num_cores = multiprocessing.cpu_count()
 
-results = Parallel(n_jobs=num_cores)(delayed(removeEmoji)(i) for i in text)
+for line, row in enumerate(translatedEmoji.itertuples(), 1):  # you don't need enumerate here, but doesn't hurt.
+     for regex in regexes:
+        translatedEmoji.at[row.Index, 'text'] = re.sub(regex, '', translatedEmoji.at[row.Index, 'text'])
 
-if(numberOfLines > 1000000):
-    results = results[0:1000000]
-x.writelines(results)
 
-x.close()
 
+translatedEmoji.to_csv('updatedAgain.csv')
 
 
 

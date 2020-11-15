9e28110e9b46b6d5eb1aed4c8c791fd7d726ae9d
Walter Felipe Griebenow <30839356+WalterGriebenow@users.noreply.github.com>
2020-11-15 13:05:08 -0500

    Add files via upload

---
+++ scripts/SentimentAnalysis.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 11 22:24:39 2020
+
+@author: Walter
+"""
+import stanza
+import datetime as dt
+import pandas as pd
+
+corenlp_dir = './corenlp'
+
+# Set the CORENLP_HOME environment variable to point to the installation location
+import os
+os.environ["CORENLP_HOME"] = corenlp_dir
+
+# Import client module
+from stanza.server import CoreNLPClient
+
+threads = pd.read_csv('threads.csv',nrows = 5000, dtype = {'tweet_id': str} )
+threads["first_sentim_l"] = ""
+threads["last_sentim_l"] = ""
+
+mask = (threads["length"] > 2) &  threads["verify_alternance"] & threads["inbound_first"] & threads["verify_thread"] & threads["verify_time"]  
+thread_ok = threads[mask].copy()
+thread_ok.set_index('tweet_id', inplace = True)
+
+full_df = pd.read_csv("twcs.csv", na_filter= False, parse_dates = ['created_at'],
+                      dtype = {'tweet_id': str,'in_response_to_tweet_id': str, 'inbound':bool, 'response_tweet_id':str })
+full_df.set_index("tweet_id", inplace = True)
+
+inbound_col, tweet_col, first_sentim_col, last_sentim_col = thread_ok.columns.get_indexer(["inbound_l","tweet_l","first_sentim_l", "last_sentim_l"])
+print("Starting a server with the Python \"with\" statement...")
+with CoreNLPClient(annotators=['sentiment'], 
+                   memory='6G', endpoint='http://localhost:9001', be_quiet=True) as client:
+    for row in range(len(thread_ok)):
+        if row % 1000 == 0:
+            print("Working on thread number: ", row, "time: ", dt.datetime.now().time())
+        inbound_text, tweet_text = thread_ok.iloc[row,[inbound_col,tweet_col]]
+        inbound_list = inbound_text.split("|")
+        tweet_list = tweet_text.split("|")
+        first_user_tweet = tweet_list[0]
+        last_user_tweet = tweet_list[::-1][inbound_list[::-1].index("True")]
+        first_tweet_text = full_df.loc[first_user_tweet,"text"]
+        last_tweet_text = full_df.loc[last_user_tweet,"text"]
+        first_doc = client.annotate(first_tweet_text)
+        last_doc = client.annotate(last_tweet_text)
+        first_sentiment = []
+        last_sentiment = []
+        for sentence in first_doc.sentence:
+            first_sentiment.append(sentence.sentiment)
+        for sentence in last_doc.sentence:
+            last_sentiment.append(sentence.sentiment)
+        thread_ok.iloc[row,[first_sentim_col,last_sentim_col]]= ['|'.join(first_sentiment),'|'.join(last_sentiment)]
+print("\nThe server should be stopped upon exit from the \"with\" statement.")
+
+thread_ok.to_csv(r'thread_first_last_sentiment.csv')  
